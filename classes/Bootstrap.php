e2d800634033973db8e8a512869ecb5d45ba6955
Liam Brydon <liam.brydon@live.com.au>
2020-05-25 22:28:30 +1200

    Updated Bootstrap class to allow controller class detection

--- classes/Bootstrap.php
+++ classes/Bootstrap.php
@@ -17,4 +17,28 @@ class Bootstrap{
             $this->action = $this->request['action'];
         }
     }
+
+    public function createController() {
+        if(class_exists($this->controller)){
+            $parents = class_parents($this->controller);
+
+            // Check extend
+            if(in_array("Controller", $parents)){
+                if(method_exists($this->controller, $this->action)){
+                    return new $this->controller($this->action, $this->request);
+                } else {
+                    //Method does not exist
+                    echo '<h1>Method does not exist</h1>';
+                    return;
+                }
+            } else {
+                //Base controller not found
+                echo '<h1>Base controller not found</h1>';
+                return;
+            }
+        } else {
+            echo '<h1>Controller class does not exist</h1>';
+                return;
+        }
+    }
 }
--- index.php
+++ index.php
@@ -1,56 +1,7 @@
 <?php
-	require 'classes/Database.php';
+	require('config.php');
+	require('classes/Bootstrap.php');
 
-	$database = new Database;
-	$post = filter_input_array(INPUT_POST);
-
-	if ($_POST['delete']){
-		$delete_id = $_POST['delete_id'];
-		$database->query('DELETE FROM posts WHERE id = :id');
-		$database->bind(':id', $delete_id);
-		$database->execute();
-	}
-	if($_POST['submit']){
-		$id = $post['id'];
-		$title = $post['title'];
-		$body = $post['body'];
-		
-		$database->query('UPDATE posts SET title = :title, body = :body WHERE id = :id');
-		$database->bind(':id', $id);
-		$database->bind(':title', $title);
-		$database->bind(':body', $body);
-		$database->execute();
-		if($database->lastInsertId()){
-			echo '<p>Post Added</p>';
-		}
-	}
-
-	$database->query('SELECT * FROM posts');
-	$rows = $database->resultset();
-?>
-
-
-
-	<h1>Add Post</h1>
-	<form method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
-		<label>Post ID</label><br />
-		<input type="text" name="id" placeholder="Specify ID" /><br />
-		<label>Post Title</label><br />
-		<input type="text" name="title" placeholder="Add a Title..." /><br />
-		<label>Post Body</label><br />
-		<textarea name="body"></textarea>
-		<input type="submit" name="submit" value="Submit" />
-	<div>
-	<h1>Posts</h1>
-	<?php foreach($rows as $row): ?>
-		<div>
-			<h3><?php echo $row['title']; ?></h3>
-			<p><?php echo $row['body']; ?><p>
-			<br />
-			<form method='post' action="<?php $_SERVER['PHP_SELF']; ?>">
-				<input type="hidden" name="delete_id" value="<?php echo $row['id']; ?>">
-				<input type="submit" name="delete" value="Delete" />
-			</form>
-		</div>
-	<?php endforeach; ?>
-	</div>
+	$bootstrap = new Bootstrap($_GET);
+	$controller = $bootstrap->createController();
+	
