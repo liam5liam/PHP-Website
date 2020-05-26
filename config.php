b18eb5e733c5911282c384713a226ce0166a9371
Liam Brydon <liam.brydon@live.com.au>
2020-05-26 21:31:45 +1200

    Added config.php file

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+// Define DB Params
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "mysql");
+define("DB_NAME", "shareboard");
+
+
+define("DB_PATH", "/");
+define("DB_URL", "http://localhost/php.ddv");
